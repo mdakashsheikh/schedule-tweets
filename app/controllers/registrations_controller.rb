a2d8f6bda9d80808d00deb963be676f45e79728c
MdAkashSheikh <nmakashsheikh@gmail.com>
2026-04-10 04:08:47 +0600

    added session

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,19 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    render plain: params[:user]
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "User created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
